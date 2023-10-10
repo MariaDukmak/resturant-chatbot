be46d457c330cb0b7c3e3894b125c2c06a8ddf1d
maryalalaland <marya@lalaland.ai>
2023-10-11 00:11:54 +0200

    Upadated datasaving logic + secrets TODO: Add evaluation of the dialog Add system responses Test

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
 import streamlit as st
-from streamlit_chat import message
 import time
+import uuid
+from google.oauth2 import service_account
+import gspread
+import pandas as pd
 
 
-def generate_response(user_unput, mode):
+def get_id():
+    # Generate a random UUID
+    random_id = uuid.uuid4()
+    # Convert the UUID to a string
+    random_id_str = str(random_id)
+    return random_id_str
+
+
+def generate_response(user_input, mode):
     return 'Still working on it!'
+
 
 def empty(placeholder):
     placeholder.empty()
     print('empty')
     time.sleep(1)
+
+
+def save_data(id, dialog, evaluation_data):
+    print("dialog", dialog)
+    data = [{'id':id, 'dialouge': dialog, 'ev':evaluation_data}]
+    df = pd.DataFrame(data, columns=['id', 'dialouge', 'ev'])
+    print(df.head())
+    return df
+
 
 def app() -> None:
     st.set_page_config(
@@ -20,8 +41,22 @@ def app() -> None:
     st.sidebar.title("Restaurants Chatbot")
     st.sidebar.image('chatbot.jpg')
 
-    st.sidebar.write(" After completing the experiment, please switch to page 2 and fill in the evaluation form")
+    st.sidebar.write("After completing the experiment, please switch to page 2 and fill in the evaluation form")
     placeholder = st.empty()
+    id = get_id()
+    user_dict = []
+    system_dict = []
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"
+        ],
+    )
+    client = gspread.authorize(credentials)
+    sheet_id = '1Xad8Afp0sRP98zARuHqPHdDCTLdnyTaB_KxfKOnGtpc'
+    csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+    sheet = client.open_by_url(csv_url).sheet1
+
     with st.sidebar:
         add_radio = st.checkbox("Ready to fill in the evaluation?", value=False)
     if not add_radio:
@@ -46,23 +81,17 @@ def app() -> None:
         if "messages" not in st.session_state:
             st.session_state.messages = []
 
-        # Display chat messages from history on app rerun
-        for message in st.session_state.messages:
-            # with chat_container.container():
-
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
-
         # Accept user input
         if prompt := st.chat_input("What is up?"):
             # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.session_state.messages.append({"user": prompt})
+            user_dict.append(prompt)
             # Display user message in chat message container
             with st.chat_message("user"):
                 st.markdown(prompt)
 
             # Display assistant response in chat message container
-            with st.chat_message("assistant"):
+            with st.chat_message("system"):
                 message_placeholder = st.empty()
                 full_response = ""
                 assistant_response = generate_response(prompt, None)
@@ -73,13 +102,13 @@ def app() -> None:
                     # Add a blinking cursor to simulate typing
                     message_placeholder.markdown(full_response + "▌")
                 message_placeholder.markdown(full_response)
+                system_dict.append(full_response)
             # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": full_response})
-            print(st.session_state)
-
+            st.session_state.messages.append({"system": full_response})
+            print(st.session_state.messages)
     if add_radio:
         empty(placeholder)
 
-    # with st.container():
-
-app()
+        database_df = save_data(id, st.session_state.messages, None)
+        database_df = database_df.astype(str)
+        sheet.insert_rows(database_df.values.tolist(), len(sheet.get_all_records()) + 2)
