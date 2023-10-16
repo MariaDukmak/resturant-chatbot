ffb33f405b8207df74e229be482b7ec85b0e1d15
maryalalaland <marya@lalaland.ai>
2023-10-16 21:45:38 +0200

    Need fix to rerun the program after every good bye

--- dialog_management/config.py
+++ dialog_management/config.py
@@ -2,7 +2,7 @@ global rules
 rules = {
     "random_responses": True,
     "anthropomorphic_response": False,
-    "restart": False,
+    "restart": True,
     "allow_change_preferences": True,
     "levenshtein": True,
     "all_caps": False,
